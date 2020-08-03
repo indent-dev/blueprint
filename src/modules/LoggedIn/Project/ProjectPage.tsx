983d6242d0037f7976e37a894a25d3cc58ab1469
Naufal Yukafi <56672065+naufalyukafi@users.noreply.github.com>
2020-08-03 10:54:26 +0700

    feat(project): add project search and sort (#17)
    * add project-filter
    
    * update project filter
    
    * fix label in project filter
    
    * fix margin label in project filter

--- src/modules/LoggedIn/Project/ProjectPage.tsx
+++ src/modules/LoggedIn/Project/ProjectPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Layout, Typography, Row, Col } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import ProjectForm from "./ProjectForm";
+import ProjectFilter from "./ProjectFilter";
 import ProjectList from "./ProjectList";
 
 const ProjectPage = () => {
@@ -20,8 +21,19 @@ const ProjectPage = () => {
       </Layout.Header>
       <Layout.Content style={{ padding: 50 }}>
         <Row align="middle" justify="space-between">
-          <Typography.Title level={4}>Project Workspace</Typography.Title>
-          <ProjectForm />
+          <Col>
+            <Typography.Title level={4}>Project Workspace</Typography.Title>
+          </Col>
+          <Col>
+            <Row align="middle" justify="space-between" gutter={16}>
+              <Col>
+                <ProjectFilter />
+              </Col>
+              <Col>
+                <ProjectForm />
+              </Col>
+            </Row>
+          </Col>
         </Row>
         <ProjectList />
       </Layout.Content>
