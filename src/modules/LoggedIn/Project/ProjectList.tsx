d008e7767a429cf59b83125e807263a7ab249bca
muhammadshiva <62060912+muhammadshiva@users.noreply.github.com>
2020-08-19 06:29:45 +0700

    feat(project): show more button (#18)
    * show more button
    
    * show-more-button
    
    Co-authored-by: M Nindra Zaka <mnindrazaka@gmail.com>

--- src/modules/LoggedIn/Project/ProjectList.tsx
+++ src/modules/LoggedIn/Project/ProjectList.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Card, Row, Col } from "antd";
+import { Card, Row, Col, Button, Layout } from "antd";
 import { useProjectSync } from "./useProjectSync";
 import { Popconfirm } from "antd";
-import { DeleteOutlined } from "@ant-design/icons";
+import { DeleteOutlined, AlignCenterOutlined } from "@ant-design/icons";
 
 const ProjectList = () => {
   const projectSync = useProjectSync();
@@ -15,54 +15,59 @@ const ProjectList = () => {
   );
 
   return (
-    <Row style={{ marginTop: 16 }} gutter={[24, 16]}>
-      {projectSync.isValidating ? (
-        <>
-          <Col span={6}>
-            <Card loading></Card>
-          </Col>
-          <Col span={6}>
-            <Card loading></Card>
-          </Col>
-          <Col span={6}>
-            <Card loading></Card>
-          </Col>
-          <Col span={6}>
-            <Card loading></Card>
-          </Col>
-        </>
-      ) : (
-        projectSync.projects.map(project => {
-          return (
-            <Col span={6} key={project._id}>
-              <Card
-                hoverable
-                actions={[
-                  <Popconfirm
-                    okButtonProps={{ loading: projectSync.isMutating }}
-                    title="Are you sure delete this project ?"
-                    onConfirm={() => confirm(project._id)}
-                    okText="Yes"
-                    cancelText="No"
-                  >
-                    <DeleteOutlined
-                      style={{
-                        fontSize: 20,
-                      }}
-                    />
-                  </Popconfirm>,
-                ]}
-              >
-                <Card.Meta
-                  title={project.name}
-                  description={project.description}
-                />
-              </Card>
+    <>
+      <Row style={{ marginTop: 16 }} gutter={[24, 16]}>
+        {projectSync.isValidating ? (
+          <>
+            <Col span={6}>
+              <Card loading></Card>
             </Col>
-          );
-        })
-      )}
-    </Row>
+            <Col span={6}>
+              <Card loading></Card>
+            </Col>
+            <Col span={6}>
+              <Card loading></Card>
+            </Col>
+            <Col span={6}>
+              <Card loading></Card>
+            </Col>
+          </>
+        ) : (
+          projectSync.projects.map(project => {
+            return (
+              <Col span={6} key={project._id}>
+                <Card
+                  hoverable
+                  actions={[
+                    <Popconfirm
+                      okButtonProps={{ loading: projectSync.isMutating }}
+                      title="Are you sure delete this project ?"
+                      onConfirm={() => confirm(project._id)}
+                      okText="Yes"
+                      cancelText="No"
+                    >
+                      <DeleteOutlined
+                        style={{
+                          fontSize: 20,
+                        }}
+                      />
+                    </Popconfirm>,
+                  ]}
+                >
+                  <Card.Meta
+                    title={project.name}
+                    description={project.description}
+                  />
+                </Card>
+              </Col>
+            );
+          })
+        )}
+      </Row>
+      <Row style={{ padding: 50 }} align="middle" justify="center">
+        <Button>Show More Project</Button>
+      </Row>
+    </>
   );
 };
 
